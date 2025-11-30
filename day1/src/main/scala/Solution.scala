edaed308789bfd848851718d83607362f4e636ff
Marbeck <raphael@watchnext.fr>
2025-11-30 19:22:09 +0100

    day 1

---
+++ day1/src/main/scala/Solution.scala
@@ -0,0 +1,17 @@
+object Solution:
+  def run(inputLines: Seq[String]): (String, String) =
+
+
+    //
+    // Code is here
+    //
+
+
+
+    val result1 = s""
+    val result2 = s""
+
+    (result1.toString, result2.toString)
+
+end Solution
+
