ec20b0ca2691b1328c73b6acc3af71611ebcd017
rmarbeck <rafoufou>
2025-12-03 09:39:02 +0100

    Day 02 - small optimizing

--- day02/src/main/scala/Solution.scala
+++ day02/src/main/scala/Solution.scala
@@ -1,5 +1,6 @@
 import scala.collection.mutable
 
+type BothResults = (Long, Long)
 
 object Solution:
   def run(inputLines: Seq[String]): (String, String) =
@@ -10,21 +11,20 @@ object Solution:
     val (result1, result2) = inputLines.head.split(",").par.collect:
       case s"$start-$end" =>
         (start.toLong to end.toLong).foldLeft((0L, 0L)):
-          case (acc, index) =>
-            val toAdd1 = if index.isInvalid then index else 0
-            val toAdd2 = if index.isInvalidPart2 then index else 0
-            acc + (toAdd1, toAdd2)
+          case (acc, index) if index.isInvalidPart1And2 => acc + (index, index)
+          case (acc, index) if index.isInvalidPart2Only => acc + (0, index)
+          case (acc, _)                                 => acc
     .reduce(_ + _)
 
     (result1.toString, result2.toString)
 
 end Solution
 
-extension (tuple: (Long, Long))
-  def +(other: (Long, Long)): (Long, Long) = (tuple._1 + other._1, tuple._2 + other._2)
+extension (tuple: BothResults)
+  def +(other: BothResults): BothResults = (tuple._1 + other._1, tuple._2 + other._2)
 
 extension (index: Long)
-  def isInvalid: Boolean =
+  def isInvalidPart1And2: Boolean =
     val asStr = index.toString
     asStr.length match
       case value if value % 2 == 0 =>
@@ -32,7 +32,7 @@ extension (index: Long)
         first == last
       case _  => false
 
-  def isInvalidPart2(using cache: mutable.Map[Int, Set[Int]]): Boolean =
+  def isInvalidPart2Only(using cache: mutable.Map[Int, Set[Int]]): Boolean =
     val asStr = index.toString
     def splitMatch(partsSize: Int): Boolean =
       val groups = asStr.grouped(asStr.length / partsSize).toSeq
@@ -43,9 +43,9 @@ extension (index: Long)
 def divisorsCached(until: Int)(using cache: mutable.Map[Int, Set[Int]]): Set[Int] =
   cache.getOrElseUpdate(until, divisors(until))
 
-def divisors(until: Int): Set[Int] = primes(until).filter(until % _ == 0)
+def divisors(until: Int): Set[Int] = primesBut2(until).filter(until % _ == 0)
 
-def primes(until: Int): Set[Int] = (2 to until).filter(isPrime).toSet
+def primesBut2(until: Int): Set[Int] = (3 to until).filter(isPrime).toSet
 
-def isPrime(n: Int) =
+def isPrime(n: Int): Boolean =
   List.range(2, n) forall (x => n % x != 0)
