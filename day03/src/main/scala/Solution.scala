8da564a5b7f0dc86865fff04ecd5705026c39786
rmarbeck <rafoufou>
2025-12-03 09:28:02 +0100

    Day03 OK

--- day03/src/main/scala/Solution.scala
+++ day03/src/main/scala/Solution.scala
@@ -1,8 +1,9 @@
 import scala.annotation.tailrec
 
+type BothResults = (Long, Long)
+
 object Solution:
   def run(inputLines: Seq[String]): (String, String) =
-
 
     val (result1, result2) = inputLines.map(findMax).reduce(_ + _)
 
@@ -11,29 +12,21 @@ object Solution:
 end Solution
 
 def findMax(bank: String): (Long, Long) =
-   val asDigits = bank.map(_.asDigit).toList
+   val asDigits = bank.map(_.asDigit)
    (findMaxRec(asDigits, 2), findMaxRec(asDigits, 12))
 
-
 @tailrec
-def findMaxRec(bank: List[Int], remaining: Int = 2, found: Long = 0L): Int =
+def findMaxRec(bank: Seq[Int], remaining: Int, alreadyFound: Long = 0L): Long =
   remaining match
-    case 0 => found 
+    case 0 => alreadyFound
     case _ => 
-      val (first, indexFirst) = findFirstDigit(bank.dropRight(remaining - 1), 0, 0, 0)
-      findMaxRec(bank.drop(indexFirst + 1), remaining - 1, found * 10 + first)
+      val (best, indexFirst) = findBestFirstDigit(bank.dropRight(remaining - 1))
+      findMaxRec(bank.drop(indexFirst + 1), remaining - 1, alreadyFound * 10 + best)
 
+inline def findBestFirstDigit(bank: Seq[Int]): (Int, Int) = bank.zipWithIndex.maxBy(_._1)
 
-@tailrec
-def findFirstDigit(bank: List[Int], best: Int, index: Int, bestIndex: Int): (Int, Int) =
-  bank match
-    case Nil => (best, bestIndex)
-    case head :: tail if head == 9 => (9, index)
-    case head :: tail if head > best => findFirstDigit(tail, head, index + 1, index)
-    case head :: tail => findFirstDigit(tail, best, index + 1, bestIndex)
-
-extension (tuple: (Long, Long))
-  def +(other: (Long, Long)): (Long, Long) = (tuple._1 + other._1, tuple._2 + other._2)
+extension (tuple: BothResults)
+  def +(other: BothResults): BothResults = (tuple._1 + other._1, tuple._2 + other._2)
 
 
 
--- day03/src/test/scala/MySuite.scala
+++ day03/src/test/scala/MySuite.scala
@@ -3,10 +3,10 @@
 class MySuite extends munit.FunSuite:
   test("Day03 : main test on final data"):
     val (score1, score2) = Solver.solve
-    assertEquals(score1, "")
-    assertEquals(score2, "")
+    assertEquals(score1, "17113")
+    assertEquals(score2, "169709990062889")
 
   test("Day03 : main test on initial data"):
     val (score1, score2) = Solver.solveTest
-    assertEquals(score1, "")
-    assertEquals(score2, "")
+    assertEquals(score1, "357")
+    assertEquals(score2, "3121910778619")
