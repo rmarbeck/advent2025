c2907aa1878347cdc8cb92b4f503f8d542d2a7de
rmarbeck <rmarbeck@users.noreply.github.com>
2025-12-05 08:44:39 +0100

    Update Solution.scala for part2

--- day05/src/main/scala/Solution.scala
+++ day05/src/main/scala/Solution.scala
@@ -10,7 +10,7 @@ object Solution:
     val result1 = figures.count: figure =>
       ranges.exists(_.contains(figure))
 
-    val result2 = s""
+    val result2 = merge(ranges).count
 
     (result1.toString, result2.toString)
 
@@ -18,3 +18,17 @@ end Solution
 
 case class LongRange(start: Long, end: Long):
   def contains(other: Long): Boolean = other >= start && other <= end
+  def overlaps(otherRange: LongRange): Boolean =
+otherRange.contains(start) || otherRange.contains(end)
+  def count: Long = end - start + 1
+  def mergeWith(otherRange: LongRange) = LongRange(Math.min(start, otherRange.start), Math.max(end, otherRange.end))
+
+def merge(toMerge: Seq[LongRange], merged: Seq[LongRange]): Seq[LongRange] =
+  if toMerge.isEmpty then
+    merged
+  else
+    val (head, tail) = (toMerge.head, toMerge.tail)
+    val (notTouched, toMergeWith) = merged.partition(!_.contains(head))
+    val mergedWithHead = toMergeWith.foldLeft(head):
+    case (acc, toMergeWith) => acc.mergeWith(toMergeWith)
+    merge(tail, notTouched ++ mergedWithHead)
