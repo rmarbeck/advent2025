3cebdc486d157a5843e9ec7f7bdd71ff646c17ec
Marbeck <raphael@watchnext.fr>
2025-12-07 09:46:57 +0100

    Day07 Part2 OK

--- day07/src/main/scala/Solution.scala
+++ day07/src/main/scala/Solution.scala
@@ -4,32 +4,32 @@ object Solution:
   def run(inputLines: Seq[String]): (String, String) =
 
     val manifold = inputLines.toArray.map(_.toCharArray)
+    val beams = manifold.map:
+      _.map:
+        case 'S' => 1L
+        case _ => 0L
 
     val start = manifold(0).indexWhere(_ == 'S')
     manifold(0)(start) = '|'
 
-    val (result1, result2) = countSplits(manifold, 0, Seq(1))
+    val (result1, result2) = countSplits(manifold, beams)
 
     (result1.toString, result2.toString)
 
 end Solution
 
 @tailrec
-def countSplits(remainingManifold: Array[Array[Char]], splits: Int, newPaths: Seq[Long]): (Int, Long) =
-  if (remainingManifold.length == 1) {
-    println(newPaths.filter(_ != 0L))
-    (splits, newPaths.filter(_ != 0L).product)
-  } else
-    val beams = remainingManifold(0).zipWithIndex.filter(_._1 == '|').map(_._2)
-    val newSplits = beams.count(remainingManifold(1)(_) == '^')
-    beams.foreach: beam =>
+def countSplits(remainingManifold: Array[Array[Char]], beamsPart2: Array[Array[Long]], splits: Int = 0): (Int, Long) =
+  if (remainingManifold.length == 1)
+    (splits, beamsPart2(0).sum)
+  else
+    val beamsPositionInPreviousLevel = beamsPart2(0).zipWithIndex.filter(_._1 > 0).map(_._2)
+    val newSplits = beamsPositionInPreviousLevel.count(remainingManifold(1)(_) == '^')
+    beamsPositionInPreviousLevel.foreach: beam =>
       if (remainingManifold(1)(beam) == '^')
-        remainingManifold(1)(beam - 1) = '|'
-        remainingManifold(1)(beam + 1) = '|'
+        beamsPart2(1)(beam - 1) += beamsPart2(0)(beam)
+        beamsPart2(1)(beam + 1) += beamsPart2(0)(beam)
       else
-        remainingManifold(1)(beam) = '|'
+        beamsPart2(1)(beam) += beamsPart2(0)(beam)
 
-    val pathsEvo = remainingManifold(1).count(_ == '|') - remainingManifold(0).count(_ == '|')
-
-
-    countSplits(remainingManifold.tail, newSplits + splits, pathsEvo +: newPaths)
+    countSplits(remainingManifold.tail, beamsPart2.tail, newSplits + splits)
--- day07/src/test/scala/MySuite.scala
+++ day07/src/test/scala/MySuite.scala
@@ -3,10 +3,10 @@
 class MySuite extends munit.FunSuite:
   test("Day07 : main test on final data"):
     val (score1, score2) = Solver.solve
-    assertEquals(score1, "")
-    assertEquals(score2, "")
+    assertEquals(score1, "1600")
+    assertEquals(score2, "8632253783011")
 
   test("Day07 : main test on initial data"):
     val (score1, score2) = Solver.solveTest
-    assertEquals(score1, "")
-    assertEquals(score2, "")
+    assertEquals(score1, "21")
+    assertEquals(score2, "40")
