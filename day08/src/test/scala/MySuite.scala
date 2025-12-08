525c8475288a1890a6ecdf16b8c4c1fb8f39c2a6
rmarbeck <rafoufou>
2025-12-08 12:30:32 +0100

    Day08 OK

--- day08/src/test/scala/MySuite.scala
+++ day08/src/test/scala/MySuite.scala
@@ -3,10 +3,10 @@
 class MySuite extends munit.FunSuite:
   test("Day08 : main test on final data"):
     val (score1, score2) = Solver.solve
-    assertEquals(score1, "")
-    assertEquals(score2, "")
+    assertEquals(score1, "46398")
+    assertEquals(score2, "8141888143")
 
   test("Day08 : main test on initial data"):
     val (score1, score2) = Solver.solveTest
-    assertEquals(score1, "")
-    assertEquals(score2, "")
+    assertEquals(score1, "40")
+    assertEquals(score2, "176904")
